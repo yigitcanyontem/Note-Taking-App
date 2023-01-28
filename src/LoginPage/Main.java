5873b5ba7fd5497697ad960b5381a9dc9a5c7eb7
Yigit148 <tekes1481@gmail.com>
2023-01-28 13:59:52 +0300

    added jar

--- src/LoginPage/Main.java
+++ src/LoginPage/Main.java
@@ -9,7 +9,6 @@ public class Main {
     static MainFrame mainFrame;
     public static void main(String[] args) throws SQLException {
         SwingUtilities.invokeLater(new Runnable() {
-
             public void run() {
 
                 new MainFrame();
